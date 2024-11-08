862d85a121e7b24fee256e70516fcd1e35e1d5bb
Luiz Fernando <aulas-programacao@bol.com.br>
2024-11-08 20:19:53 -0300

    cls

--- app.js
+++ app.js
@@ -5,29 +5,38 @@
 // variavei de apoio logico
 let chave = false // o interruptor inicia desligado
 let lampada = true // a lâmpada está OK
+// Pré carregamento do arquivo de áudi
+let som = new Audio("sound/breaking-glass.mp3")
+
 function quebrar() {
-    if(lampada === true)
-    document.getElementById('lamp').src="img/broken.jpg"
+    if (lampada === true)
+        document.getElementById('lamp').src = "img/broken.jpg"
     //reproduzindo um arquivo de áudio no JS
     //Passo 1: copiar o arquivo de áudio para o projeto
     // Passo 2: usar a classe Audio(biblioteca interna do JS)
-    let som = new Audio()
-    som.src = "sound/glassbreaking.wav"
+    // Passo 3: pré carregar o arquivo fr áudio para sincronizar com a troca de imagem (Experência do Usuário)
+    
     som.play()
     // apoio a lógica para o JS identificar a lâmpada quebrada
-    lampada = false    
+    lampada = false
 }
 function onoff() {
-    if (chave === false && lampada === true){
+    if (chave === false) {
         // ligar a chave
         document.getElementById('interruptor').src = "img/swon.png"
         chave = true // O JS agora sabe que a chave está ligada
-        //acender a lâmpada
-        document.getElementById('lamp').src = "img/on.jpg"
-    } else if (lampada === true){
+        //verificar se a lâmpada está intacta antes de acender
+        if (lampada === true) {
+            document.getElementById('lamp').src = "img/on.jpg"
+        }
+    } else {
         document.getElementById('interruptor').src = "img/swoff.png"
         chave = false
-        document.getElementById('lamp').src = "img/off.jpg"
+        //verificar se a lâmpada está intacta antes de apagar
+        if (lampada === true) {
+            document.getElementById('lamp').src = "img/off.jpg"
+        }
     }
-    
 }
+
+// Estudo de eventos relacionados a click do mouse (pressionado ou não pressionado) e telas touch
